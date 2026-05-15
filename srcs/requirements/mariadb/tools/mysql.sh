790e30f9fe7176192f2f4ad69b867328079e0fb6
crappo <crappo@crappo.crappo.42.fr>
2026-05-15 11:18:34 +0200

    Update the Inception project adding the wordpress contaner and the docker-compose.yml

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -1,8 +1,23 @@
 
+# service mariadb start;
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "FLUSH PRIVILEGES;"
+# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# exec mysqld_safe
+
+
+# Start Mysql
 service mariadb start;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+sleep 2.5
+
+# Set database
+mysql -e "CREATE DATABASE IF NOT EXISTS test;"
+mysql -e "CREATE USER IF NOT EXISTS crappo@'localhost' IDENTIFIED BY 'password';"
+mysql -e "GRANT ALL PRIVILEGES ON test.* TO crappo@'%' IDENTIFIED BY 'password';"
 mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+mysqladmin -u root -proot_password shutdown
+
 exec mysqld_safe
