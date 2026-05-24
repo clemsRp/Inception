253a535c20f55a847c8cb00b6caa495b34e83c47
crappo <clement.rappo@edmu.fr>
2026-05-24 09:24:14 +0200

    Fixed terminal errors, need to fix ports

--- srcs/requirements/mariadb/tools/mysql_config.sh
+++ srcs/requirements/mariadb/tools/mysql_config.sh
@@ -1,16 +1,23 @@
+#!/bin/bash
 
-# Start Mysql
-service mariadb start;
-sleep 2.5
+SQL_PASSWORD=$(cat /run/secrets/db_password)
+SQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-# Set database
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+    service mariadb start
+    sleep 2
+
+    mariadb -u root <<EOF
+CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+    mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
+    sleep 2
+fi
 
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/php_config.sh
+++ srcs/requirements/wordpress/tools/php_config.sh
@@ -5,17 +5,35 @@ sleep 10
 # Get the wordpress variables
 . /run/secrets/credentials
 
-# Generate the wp-config.php file
-wp config create \
-	--allow-root \
-	--dbname=$WORDPRESS_DATABASE \
-	--dbuser=$WORDPRESS_USER \
-	--dbpass=$WORDPRESS_PASSWORD \
-	--dbhost=mariadb:3306 \
-	--path='/var/www/wordpress' \
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	wp core download \
+		--path='/var/www/wordpress' \
+		--allow-root --force
 
-# Install the wordpress core and create the first user, defined previously in the wp-config.php
-wp core install --allow-root
+	# Generate the wp-config.php file
+	wp config create \
+		--allow-root \
+		--dbname=$WORDPRESS_DATABASE \
+		--dbuser=$WORDPRESS_USER \
+		--dbpass=$WORDPRESS_USER_PASSWORD \
+		--dbhost=mariadb:3306 \
+		--path='/var/www/wordpress'
 
-# Create the second user in the database
-wp user create --allow-root
+	# Install the wordpress core and create the first user, defined previously in the wp-config.php
+	wp core install \
+		--allow-root \
+		--path='/var/www/wordpress' \
+		--admin_user=$ADMIN_USER \
+		--url=$COMMON_NAME \
+		--title=$WEBSITE_NAME \
+		--admin_email=$WORDPRESS_ADMIN_EMAIL
+
+	# Create the second user in the database
+	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL \
+	    --allow-root \
+	    --path='/var/www/wordpress' \
+	    --role=author \
+	    --user_pass=$WORDPRESS_USER_PASSWORD
+fi
+
+exec php-fpm7.4 -F
